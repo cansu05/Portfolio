677f84d1a2259f2ec67006c9300d6f55b46ca067
Cansu uğur <99813607+cansu05@users.noreply.github.com>
2025-07-13 14:46:44 +0300

    navbar bug fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
 import NavLinks from "./NavLinks";
 
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav className="bg-white h-20 lg:h-screen  flex lg:flex-col flex-row items-center justify-center gap-3">
+    <nav
+      className={` transition-colors duration-300 ${
+        scrolled ? "lg:bg-white bg-gray-100  " : "bg-white"
+      } h-20 lg:h-screen flex lg:flex-col flex-row items-center justify-center gap-3`}
+    >
       <NavLinks />
     </nav>
   );
 };
+
 export default Navbar;
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router";
 const MainLayout = () => {
   return (
     <div className="flex lg:flex-row flex-col min-h-screen">
-      <div className="lg:w-96 w-full">
+      <div className="lg:w-72 w-full fixed z-50">
         <Navbar />
       </div>
       <div className="w-full bg-stone-100 px-20">
